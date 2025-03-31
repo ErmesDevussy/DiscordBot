ce68aee812e9fb6d84f86e5fa35cb3f328a90bbe
Chell Bell <ermes.jabba2137@gmail.com>
2025-03-31 17:50:36 +0200

    progress

--- src/main/java/me/chellbell/discordBot.java
+++ src/main/java/me/chellbell/discordBot.java
@@ -1,39 +1,22 @@
 package me.chellbell;
 
-// import java.util.Collection;
-// import java.util.EnumSet;
-
-// import javax.print.DocFlavor.STRING;
 import javax.security.auth.login.LoginException;
 
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
-// import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
-// import net.dv8tion.jda.api.interactions.InteractionContextType;
-// import net.dv8tion.jda.api.interactions.commands.Command;
-// import net.dv8tion.jda.api.requests.GatewayIntent;
-// import net.dv8tion.jda.api.requests.restaction.CommandListUpdateAction;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.requests.GatewayIntent;
+import org.jetbrains.annotations.NotNull;
 
-public class discordBot {
+
+public class discordBot extends ListenerAdapter {
     public static void main(String[] args) throws LoginException {
         JDA jda = JDABuilder.createDefault("")
-            .setActivity(Activity.playing("Balling"))
-            // .addEventListeners(new SlahsBotExpample())
-            .build();
-
-    //     CommandListUpdateAction commands = jda.updateCommands();
-
-    //     commands.addCommands(
-    //         Commands.slash("say", "Dayum")
-    //             .setContexts(InteractionContextType.ALL)
-    //             .addOption(STRING, "content", "What the bot should say", true)
-    //     );
-
-    //     commands.queue();
-    // }
-
-    // public void say(SlashCommandInteractionEvent event, String content) {
-    //     event.reply(content).queue();
+                .enableIntents(GatewayIntent.MESSAGE_CONTENT)
+                .setActivity(Activity.playing("Balling"))
+                .addEventListeners(new BotListeners())
+                .build();
     }
 }
